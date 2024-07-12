c5562eb86f221c67deb77ab32c7dafac9e506af1
Zirdgal <zirdman@gmail.com>
2024-07-12 04:16:19 +0300

    Midas' Giftfx pt2

--- scripts/rarity-handler-script.js
+++ scripts/rarity-handler-script.js
@@ -6,7 +6,11 @@ export function getRarity() {
     let rarityNumber = Math.random();
     console.log(rarityNumber);
 
-    if (rarityNumber <= 0.5) {
+    if (rarityNumber === 0.1 || rarityNumber === 0.2 || rarityNumber === 0.3 || rarityNumber === 0.4 || rarityNumber === 0.5 || rarityNumber === 0.6 || rarityNumber === 0.7 || rarityNumber === 0.8 || rarityNumber === 0.9) {
+        rarityText.style.color = "darkgoldenrod";
+        rarityText.innerHTML = "Midas' Gift";
+        chanceText.innerHTML = "0.00001%";
+    } else if (rarityNumber <= 0.5) {
         rarityText.style.color = "darkgray";
         rarityText.innerHTML = "common";
         chanceText.innerHTML = "50%";
@@ -26,7 +30,7 @@ export function getRarity() {
         rarityText.style.color = "gold";
         rarityText.innerHTML = "The Rarest";
         chanceText.innerHTML = "0.000001%";
-    } else {
+    }  else {
         rarityText.style.color = "black";
         rarityText.innerHTML = "void";
         chanceText.innerHTML = "6.25%"
